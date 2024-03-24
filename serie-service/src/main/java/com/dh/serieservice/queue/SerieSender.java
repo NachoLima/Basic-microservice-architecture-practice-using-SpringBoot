16170b5f9f0e2a4f1f55a53e796faf017c22a7ed
Leonardo <leonardojfs83@gmail.com>
2024-03-24 02:59:41 -0300

    Implementacion de RabbitMQ, al crear una serie, se envia un mensaje con la serie creada.

---
+++ serie-service/src/main/java/com/dh/serieservice/queue/SerieSender.java
@@ -0,0 +1,19 @@
+package com.dh.serieservice.queue;
+
+import com.dh.serieservice.model.Serie;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+@Component
+@RequiredArgsConstructor
+public class SerieSender {
+    private final RabbitTemplate rabbitTemplate;
+    private final Queue catalogQueue;
+
+    public void send(Serie serie) {
+        this.rabbitTemplate.convertAndSend(this.catalogQueue.getName(), serie);
+    }
+
+}
