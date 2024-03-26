52df61574723f1ca929933582741cb440311da23
Leonardo <leonardojfs83@gmail.com>
2024-03-25 21:39:32 -0300

    Implementacion persistencia de series en Catalog service con Base de datos MongoDB Embedded

--- catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
@@ -1,9 +1,11 @@
 package com.dh.catalogservice.controller;
 
+import com.dh.catalogservice.Service.GeneroService;
 import com.dh.catalogservice.client.IMovieClient;
 import com.dh.catalogservice.client.ISerieClient;
+import com.dh.catalogservice.model.Genero;
 import com.dh.catalogservice.model.Serie;
-import com.dh.movieservice.model.Movie;
+import com.dh.catalogservice.model.Movie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +22,10 @@ public class CatalogController {
 
     @Autowired
     private ISerieClient iSerieClient;
+    @Autowired
+    private GeneroService generoService;
 
-    @GetMapping("/catalog/{genre}")
+    @GetMapping("/catalog/movie/{genre}")
     public ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre) {
         ResponseEntity<List<Movie>> movies = iMovieClient.getMovieByGenre(genre);
 
@@ -29,24 +33,25 @@ public class CatalogController {
         return movies;
     }
 
-    @PostMapping("/catalog/save")
+    @GetMapping("/catalog/{genre}")
+    public Genero getGeneroByNombre(@PathVariable String genre) {
+        return generoService.getGeneroByNombre(genre);
+    }
+
+
+    @PostMapping("/catalog/movie/save")
     public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie) {
         return iMovieClient.saveMovie(movie);
     }
 
-    @GetMapping("/catalog/series")
-    public List<Serie> getAllSeries() {
-        List<Serie> series = iSerieClient.getAll();
-
-        return series;
-    }
     @GetMapping("/catalog/series/{genre}")
     public List<Serie> getSerieByGenre(@PathVariable String genre) {
         return iSerieClient.getSerieByGenre(genre);
     }
-    @PostMapping("/catalog/series")
+    @PostMapping("/catalog/series/save")
     public String createSerie(@RequestBody Serie serie) {
         return iSerieClient.create(serie);
     }
+
 }
 
