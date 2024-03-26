955d86320a57dfddb335659505e9ffa70db8b4de
Leonardo <leonardojfs83@gmail.com>
2024-03-25 23:58:16 -0300

    Implementacion RabbitMQ Listener para Serie Service

--- catalog-service/src/main/java/com/dh/catalogservice/queue/SerieListener.java
+++ catalog-service/src/main/java/com/dh/catalogservice/queue/SerieListener.java
@@ -1,5 +1,6 @@
 package com.dh.catalogservice.queue;
 
+import com.dh.catalogservice.Service.GeneroService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,13 +10,14 @@ import com.dh.catalogservice.model.Serie;
 @Component
 public class SerieListener {
 
-    //TODO implementar CatalogService para guardar las peliculas y series en MongoDB
-    //@Autowired
-    //private final CatalogService catalogService;
+    //Inyecto el servicio de genero
+    @Autowired
+    private GeneroService generoService;
 
+    //Recibe un objeto Serie y lo guarda en la base de datos de catalogo
     @RabbitListener(queues = {"${queue.catalog.name}"})
         public void receive(@Payload Serie serie) {
-            //catalogService.save(serie);
+            generoService.saveSerie(serie);
             System.out.println("Serie received: " + serie);
         }
 
